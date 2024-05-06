400716a7630a1cdfd68ee340342a2dcd70f6e2ed
Abderlhman00 <ahmedabderlhman0@gmail.com>
2024-05-06 20:07:10 +0300

    Linding page

---
+++ resources/js/Pages/LandingPage/SectionVideoOne.vue
@@ -0,0 +1,20 @@
+<template>
+
+
+    <div class="text-center pt-20 bg-[#EAF0FF] min-h-full">
+        <h1 class="font-bold pb-10 text-[#1C2F41] text-4xl">How me?</h1>
+        <video class="overflow-hidden mx-auto h-[50%] w-[75%] rounded-lg" autoplay loop muted>
+        <source src="@/Images/WhatsApp Video 2024-04-23 at 12.26.03 AM.mp4" type="video/mp4" />
+        </video>
+        <p class="mt-20 pb-14 font-bold text-[#1C2F41] text-2xl">
+            Some representative placeholder content for the first slide
+        </p>
+
+    </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style></style>
---
+++ resources/js/Pages/LandingPage/SectionVideoTwo.vue
@@ -0,0 +1,19 @@
+<template>
+
+
+    <div class="text-center bg-[#cedafb] ">
+        <h1 class="pb-10 font-bold text-[#1C2F41] text-4xl">How me?</h1>
+        <p class=" font-bold text-[#1C2F41] pb-5 text-2xl">
+            Some representative placeholder content for the first slide.
+        </p>
+        <video class="pb-20 mx-auto h-[50%] w-[70%] rounded-lg" autoplay loop muted>
+            <source src="@/Images/WhatsApp Video 2024-04-23 at 12.26.03 AM.mp4" type="video/mp4" />
+        </video>
+    </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style></style>
