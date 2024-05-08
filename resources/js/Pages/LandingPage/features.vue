5985fb17cdf21cbe9f529c2079b61206428ade7c
Abderlhman00 <ahmedabderlhman0@gmail.com>
2024-05-09 01:32:36 +0300

    Linding page

--- resources/js/Pages/LandingPage/features.vue
+++ resources/js/Pages/LandingPage/features.vue
@@ -1,5 +1,5 @@
 <template>
-                                                <section class="py-24 ">
+<section class="py-24 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div class="mb-10 lg:mb-16 flex justify-center items-center flex-col gap-x-0 gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between max-md:max-w-lg max-md:mx-auto">
                   <div class="relative w-full text-center lg:text-left lg:w-2/4">
@@ -65,7 +65,7 @@
               </div>
         </div>
       </section>
-                                            
+
 </template>
 
 <script>
