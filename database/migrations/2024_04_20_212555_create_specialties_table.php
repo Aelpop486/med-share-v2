4e099cc4a2f74fcabbef13069cfa41eb53352994
Elmasry09 <ahmedelmasary1079@gmail.com>
2024-06-15 02:23:41 +0300

    admin dashboard v9

--- database/migrations/2024_04_20_212555_create_specialties_table.php
+++ database/migrations/2024_04_20_212555_create_specialties_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('specialties', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('description');
             $table->foreignId('admin_id')->nullable()->constrained('admins')->onDelete('set null')->onUpdate('set null');
             $table->timestamps();
